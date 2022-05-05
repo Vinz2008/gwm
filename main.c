1fd84a4e4e406cc41e1c0f99db7e75ce76c6dd36
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-05-05 16:19:40 +0200

    added text in window

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main() {
@@ -13,23 +14,10 @@ int main() {
 	xcb_generic_event_t *e;
 	uint32_t mask;
 	uint32_t values[2];
-	xcb_point_t points[] = {
-		{10, 10}, {10, 20}, {20, 10}, {20, 20}
-	};
-	xcb_point_t polyline[] = {
-		{50, 10}, {5, 20}, {25, -20}, {20, 10}
-	};
-	xcb_segment_t segments[] = {
-		{100, 10, 140, 30}, {110, 25, 130, 60}
-	};
+	char string[] = "gwm";
 	xcb_rectangle_t rectangles[] = {
-		{10, 50, 40, 20}, {80, 50, 10, 40}
-	};
-	xcb_arc_t arcs[] = {
-		{10, 100, 60, 40, 0, 90 << 6},
-		{90, 100, 55, 40, 0, 270 << 6}
-	};
-	struct xkb_context *ctx;
+    	{40, 40, 20, 20},
+  	};
 	c = xcb_connect(NULL, NULL); // connect to X server
 	screen = xcb_setup_roots_iterator(xcb_get_setup (c)).data; // get first screen
 
@@ -64,20 +52,19 @@ int main() {
 		switch (e->response_type & ~0x80)
 		{
 		case XCB_EXPOSE:
-			xcb_poly_point(c, XCB_COORD_MODE_ORIGIN, win, foreground, 4, points);
-			xcb_poly_line(c, XCB_COORD_MODE_PREVIOUS, win, foreground, 4, polyline);
-			xcb_poly_segment(c, win, foreground, 2, segments);
-			xcb_poly_rectangle(c, win, foreground, 2, rectangles);
-			xcb_poly_arc(c, win, foreground, 2, arcs);
+			xcb_poly_rectangle(c, win, foreground, 1, rectangles);
+			xcb_image_text_8 (c, strlen(string), win, foreground, 20, 20, string);
 			xcb_flush(c);
 			break;
-		
+		case XCB_KEY_PRESS:
+      		goto endloop;
+
 		default:
 			// unknown event that is ignored
 			break;
 		}
+		free(e);
 	}
-	free(e);
-	sleep(5);
+	endloop:
 	return 0;
 }
